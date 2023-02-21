99020cf021ecd8223dad6c97f24da0e57450a61f
Francis <francisbernal14@gmail.com>
2023-02-21 22:46:05 +0100

    feat: create <Display> component and add it to <export.js>

---
+++ src/components/display.js
@@ -0,0 +1,7 @@
+export const Display = (props) => {
+    return (
+        <div id="display">
+            test
+        </div>
+    )
+}
--- src/components/export.js
+++ src/components/export.js
@@ -1,3 +1,4 @@
 import { Buttons } from './buttons';
+import { Display } from './display';
 
-export { Buttons }
+export { Buttons, Display }
