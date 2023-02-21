f1f669ed0bd5a8e98a943911a4889493cb143340
Francis <francisbernal14@gmail.com>
2023-02-21 22:44:13 +0100

    fix: re-adjusts <Buttons> components inserting rows

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,21 +1,33 @@
 export const Buttons = (props) => {
     return (
         <div id="buttons-container">
-            <button onClick={props.handleClick} id="clear">AC</button>
-            <button onClick={props.handleClick} id="divide">/</button>
-            <button onClick={props.handleClick} id="multiply">X</button>
-            <button onClick={props.handleClick} id="seven">7</button>
-            <button onClick={props.handleClick} id="eight">8</button>
-            <button onClick={props.handleClick} id="nine">9</button>
-            <button onClick={props.handleClick} id="four">4</button>
-            <button onClick={props.handleClick} id="five">5</button>
-            <button onClick={props.handleClick} id="six">6</button>
-            <button onClick={props.handleClick} id="one">1</button>
-            <button onClick={props.handleClick} id="two">2</button>
-            <button onClick={props.handleClick} id="three">3</button>
-            <button onClick={props.handleClick} id="zero">0</button>
-            <button onClick={props.handleClick} id="decimal">.</button>
-            <button onClick={props.handleClick} id="equals">=</button>
+            <div id="buttons-row">
+                <button onClick={props.handleClick} id="clear">AC</button>
+                <button onClick={props.handleClick} id="divide">/</button>
+            </div>
+            <div id="buttons-row">
+                <button onClick={props.handleClick} id="seven">7</button>
+                <button onClick={props.handleClick} id="eight">8</button>
+                <button onClick={props.handleClick} id="nine">9</button>
+                <button onClick={props.handleClick} id="multiply">X</button>
+            </div>
+            <div id="buttons-row">
+                <button onClick={props.handleClick} id="four">4</button>
+                <button onClick={props.handleClick} id="five">5</button>
+                <button onClick={props.handleClick} id="six">6</button>
+                <button onClick={props.handleClick} id="subtract">-</button>
+            </div>
+            <div id="buttons-row">
+                <button onClick={props.handleClick} id="one">1</button>
+                <button onClick={props.handleClick} id="two">2</button>
+                <button onClick={props.handleClick} id="three">3</button>
+                <button onClick={props.handleClick} id="add">+</button>
+            </div>
+            <div id="buttons-row">
+                <button onClick={props.handleClick} id="zero">0</button>
+                <button onClick={props.handleClick} id="decimal">.</button>
+                <button onClick={props.handleClick} id="equals">=</button>
+            </div>
         </div>
     )
 }
